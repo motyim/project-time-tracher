0ef42c75584cf925493534e6771e9c66ced84c62
Mohamed MotYim <mohamed.motyim@gmail.com>
2019-12-14 16:02:52 +0300

    init project

---
+++ src/main/java/me/motyim/projecttimetracher/ProjectTimeTracherApplication.java
@@ -0,0 +1,13 @@
+package me.motyim.projecttimetracher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTimeTracherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTimeTracherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/motyim/projecttimetracher/ProjectTimeTracherApplicationTests.java
@@ -0,0 +1,13 @@
+package me.motyim.projecttimetracher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTimeTracherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
